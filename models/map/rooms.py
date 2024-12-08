1df5fabb687905c63b5e0c2a977ddcfcfb5a97d8
bogda <bogdashka04@gmail.com>
2024-12-08 10:15:45 +0300

    Описали клас Door файла rooms.py

--- models/map/rooms.py
+++ models/map/rooms.py
@@ -7,13 +7,23 @@ class Room:
         self.room_color = pygame.color.Color((0, 120, 255))
         self.field = Field()
 
+        # Тестовая
+        self.door = Door(400, 50)
+
     def draw(self, screen: pygame.Surface):
         self.field.draw(screen)
         pygame.draw.rect(screen, self.room_color, self.room, width=2)
 
+        self.door.draw(screen)
 
-class Door: # Пока не трогаем
-    pass
+
+class Door:
+    def __init__(self, x: int, y: int):
+        self.door = pygame.rect.Rect(x, y, 200, 50)
+        self.door_color = pygame.color.Color((0, 255, 255))
+
+    def draw(self, screen: pygame.Surface):
+        pygame.draw.rect(screen, self.door_color, self.door, width=2)
 
 
 class Field:
