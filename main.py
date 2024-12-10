065d7bf1924d472424786dc50d964567b30f4f67
kapusta-gg <bogdashka04@gmail.com>
2024-12-10 16:30:20 +0300

    Добавлено движение игрока (какой же ужас написан)

--- main.py
+++ main.py
@@ -8,13 +8,17 @@ from models.entities.player import Player
 from models.entities.enemies import *
 
 UP, DOWN, LEFT, RIGHT = 0, 1, 2, 3
+DIRECTIONS = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+KEYS_TO_IND = {pygame.K_w: UP,
+               pygame.K_a: LEFT,
+               pygame.K_d: RIGHT,
+               pygame.K_s: DOWN}
 
-def func(x):
-    if x < -1:
-        return -1
-    if x > 1:
-        return 1
-    return x
+def change_movement(ind, is_press, all_bool_movements):
+    all_bool_movements[ind] = is_press
+    x = sum(DIRECTIONS[i][0] for i in range(4) if all_bool_movements[i])
+    y = sum(DIRECTIONS[i][1] for i in range(4) if all_bool_movements[i])
+    return x, y
 
 if __name__ == '__main__':
     pygame.init()
@@ -47,24 +51,16 @@ if __name__ == '__main__':
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_u:
                     isShowHitbox = not isShowHitbox
-                if event.key == pygame.K_w:
-                    direction = (0, -1)
-                    isMovePlayer = True
-                if event.key == pygame.K_a:
-                    direction = (-1, 0)
-                    isMovePlayer = True
-                if event.key == pygame.K_s:
-                    direction = (0, 1)
-                    isMovePlayer = True
-                if event.key == pygame.K_d:
-                    direction = (1, 0)
-                    isMovePlayer = True
-                if isMovePlayer:
-                    pos = (func(pos[0] + direction[0]), func(pos[1] + direction[1]))
+                # Проверка WASD
+                [change_movement(KEYS_TO_IND[i], True, isMovePlayer)
+                 for i in [pygame.K_w, pygame.K_a, pygame.K_s, pygame.K_d] if event.key == i]
+                pos = change_movement(UP, isMovePlayer[UP], isMovePlayer)
             if event.type == pygame.KEYUP:
-                if event.key == pygame.K_w or event.key == pygame.K_a or \
-                    event.key == pygame.K_s or event.key == pygame.K_d:
-                    isMovePlayer = False
+                # Проверка WASD
+                [change_movement(KEYS_TO_IND[i], False, isMovePlayer)
+                 for i in [pygame.K_w, pygame.K_a, pygame.K_s, pygame.K_d] if event.key == i]
+                pos = change_movement(UP, isMovePlayer[UP], isMovePlayer)
+
         # Отрисовка объектов
         screen.fill((0, 0, 0))
         hud.draw(screen, is_hitbox=isShowHitbox)
@@ -74,8 +70,8 @@ if __name__ == '__main__':
 
         enemy.draw(screen, is_hitbox=isShowHitbox)
         objectt.draw(screen, is_hitbox=isShowHitbox)
-        # Выполняем логику (пока пусто)
-        if isMovePlayer:
+        # Выполняем логику
+        if any(isMovePlayer):
             player.move(*(i * tick / 1000 for i in pos))
         # Обновление экрана
         pygame.display.flip()
