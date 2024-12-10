a9ed9d849b450063394ac76f93feba91e9d5db97
kapusta-gg <bogdashka04@gmail.com>
2024-12-10 16:04:03 +0300

    Тест движения игрока

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import pygame
+
 from models.map.rooms import Room
 from models.map.objects import *
 from models.interface.minimap import MiniMap
@@ -5,6 +7,14 @@ from models.interface.hud import HUD
 from models.entities.player import Player
 from models.entities.enemies import *
 
+UP, DOWN, LEFT, RIGHT = 0, 1, 2, 3
+
+def func(x):
+    if x < -1:
+        return -1
+    if x > 1:
+        return 1
+    return x
 
 if __name__ == '__main__':
     pygame.init()
@@ -21,10 +31,15 @@ if __name__ == '__main__':
     enemy = Enemy(300, 300)
     objectt = Object(100, 100)
 
+    pos = (0, 0)
     isShowHitbox = True
+    isMovePlayer = [False, False, False, False]
+
+    clock = pygame.time.Clock()
 
     running = True
     while running:
+        tick = clock.tick()
         # Обрабатываем события
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -32,6 +47,24 @@ if __name__ == '__main__':
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_u:
                     isShowHitbox = not isShowHitbox
+                if event.key == pygame.K_w:
+                    direction = (0, -1)
+                    isMovePlayer = True
+                if event.key == pygame.K_a:
+                    direction = (-1, 0)
+                    isMovePlayer = True
+                if event.key == pygame.K_s:
+                    direction = (0, 1)
+                    isMovePlayer = True
+                if event.key == pygame.K_d:
+                    direction = (1, 0)
+                    isMovePlayer = True
+                if isMovePlayer:
+                    pos = (func(pos[0] + direction[0]), func(pos[1] + direction[1]))
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_w or event.key == pygame.K_a or \
+                    event.key == pygame.K_s or event.key == pygame.K_d:
+                    isMovePlayer = False
         # Отрисовка объектов
         screen.fill((0, 0, 0))
         hud.draw(screen, is_hitbox=isShowHitbox)
@@ -42,5 +75,7 @@ if __name__ == '__main__':
         enemy.draw(screen, is_hitbox=isShowHitbox)
         objectt.draw(screen, is_hitbox=isShowHitbox)
         # Выполняем логику (пока пусто)
+        if isMovePlayer:
+            player.move(*(i * tick / 1000 for i in pos))
         # Обновление экрана
         pygame.display.flip()
--- models/entities/player.py
+++ models/entities/player.py
@@ -2,10 +2,18 @@ import pygame
 
 
 class Player:
+    PLAYER_SPEED = 100
     def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
         self.player_hitbox = pygame.rect.Rect(x, y, 90, 90)
         self.player_color = pygame.color.Color((0, 255, 0))
 
     def draw(self, screen: pygame.Surface, is_hitbox=True):
         if is_hitbox:
             pygame.draw.rect(screen, self.player_color, self.player_hitbox, width=2)
+
+    def move(self, x, y):
+        self.x += (x * Player.PLAYER_SPEED)
+        self.y += (y * Player.PLAYER_SPEED)
+        self.player_hitbox = pygame.rect.Rect(int(self.x), int(self.y) , 90, 90)
